7ad60c5d8a74464413d89f74beefd2811acf983d
gauravp <gauravp@clavax.com>
2020-01-06 19:34:09 +0530

    changes for anchor add

--- python-rest/hrm/admin.py
+++ python-rest/hrm/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from hrm.models import User
 from django.contrib.auth.models import Group
 from rest_framework.authtoken.models import Token
+from django.utils.html import format_html
 # Register your models here.
 
 
@@ -12,10 +13,17 @@ upper_case_name.short_description = 'Name'
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username',upper_case_name, 'email','is_active','is_staff')
+    list_display = ('username',upper_case_name, 'email','is_active','is_staff','Keywordd')
     search_fields = ('first_name', 'last_name',)
     list_filter = ('is_active','is_staff')
     upper_case_name.admin_order_field = 'first_name'
+
+    def Keywordd(self, obj):
+        return format_html('<a class="user-detail" data-id =%s  href="#">%s</a>' % (obj.id, 'detail'))
+        
+    Keywordd.allow_tags = True
+    Keywordd.admin_order_field = 'ProjectKW'
+    Keywordd.short_description = 'ProjectKW'
     # ordering = ['Name']
 
 
